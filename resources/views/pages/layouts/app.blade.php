622ae10a2c5b942d5c9a7c6eaae2b79efd165b60
Gnosis <emmagab38@gmail.com>
2023-04-02 16:12:27 +0100

    commit changes

--- resources/views/pages/layouts/app.blade.php
+++ resources/views/pages/layouts/app.blade.php
@@ -176,6 +176,11 @@
 {{--    <i class="fa fa-whatsapp my-float"></i>--}}
 {{--</a>--}}
 <!-- GetButton.io widget -->
+
+
+<script>(function(d,s,id,c){var js,rC=[],uRC=[],r=0;Array.from(document.querySelectorAll('[class*="stjr-"]')).forEach(r=>{rC=[...rC,...Array.from(r.classList).filter((cl)=>{return /^stjr-/.test(cl);})]});uRC=[...new Set(rC)];t=d.getElementsByTagName(s)[0];js=d.createElement(s);js.id=id;js.src='https://www.sitejabber.com/js/v2/64296ff4a6726/widgets.js'+(uRC.length ? '?widget-classes=' + uRC.join("|") : '?widget-classes=stjr-base')+'';js.onload=js.onreadystatechange=function(){if(!r&&(!this.readyState||this.readyState[0]=='c')){r=1;c();}};t.parentNode.insertBefore(js,t);}(document,'script','sj-widget',function(){}));</script>
+
+
 <script type="text/javascript">
     (function () {
         var options = {
